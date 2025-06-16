4cc3c02c5ba40d49d21c7db712be5ff2ee722388
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-06-16 14:22:01 +0530

    DOM

--- DOM/script.js
+++ DOM/script.js
@@ -28,7 +28,52 @@
 // textContent - shows hidden element text
 
 
-let h2 = document.querySelector("h2")
-console.dir(h2)
-h2.innerText = h2.innerText + " from Shivansh"
-// h2.append("from Shivansh")
+// let h2 = document.querySelector("h2")
+// console.dir(h2)
+// h2.innerText = h2.innerText + " from Shivansh"
+// // h2.append("from Shivansh")
+
+// let divs = document.querySelectorAll(".box")
+// // divs[0].innerText = "You are 1"
+// // divs[1].innerText = "You are 2"
+// // divs[2].innerText = "You are 3"
+// let idx = 1
+// for (div of divs) {
+//     div.innerText = `You are ${idx}`
+//     idx++
+// }
+
+// let div = document.querySelector("div")
+// console.log(div.getAttribute("id"))
+// console.log(div.setAttribute("id", "boxId"))
+
+// let header = document.querySelector("h2")
+
+// header.style.backgroundColor = "green"
+
+// let newBtn = document.createElement("button")
+// newBtn.innerText = "CLick me"
+
+// let div = document.querySelector("div")
+// div.after(newBtn)
+
+// let newHeading = document.createElement("h1")
+// newHeading.innerHTML = "<i>Hi, I'm new </i>"
+
+// document.querySelector("body").prepend(newHeading)
+
+// let para = document.querySelector("p")
+// para.remove()
+
+// Assignment 
+
+let btn = document.createElement("button")
+btn.innerText = "Click Me"
+btn.style.backgroundColor = "red"
+btn.style.color = "white"
+
+document.querySelector("body").prepend(btn)
+
+let para = document.querySelector("p")
+
+para.classList.add("newClass")
