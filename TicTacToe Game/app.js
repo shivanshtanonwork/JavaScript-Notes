ce812889d959e94672c01a5b9c5a0842e7a1922c
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-06-16 22:53:00 +0530

    Tic-Tac-Toe Game

---
+++ TicTacToe Game/app.js
@@ -0,0 +1,95 @@
+/**
+ * 8 winning patterns - if 1 of it is true - we have a winner
+ */
+
+let boxes = document.querySelectorAll(".box")
+let resetBtn = document.querySelector("#reset-btn")
+let newGameBtn = document.querySelector("#new-btn")
+let msgContainer = document.querySelector(".msg-container")
+let msg = document.querySelector("#msg")
+
+let turnO = true //playerX, playerY
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+]
+
+const resetGame = () => {
+    turnO = true
+    enableBoxes()
+    msgContainer.classList.add("hide")
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turnO) {
+            box.innerText = "O"
+            box.classList.add("O-style")
+            turnO = false
+        } else {
+            box.innerText = "X"
+            box.classList.add("X-style")
+            turnO = true
+        }
+        box.disabled = true
+
+        checkWinner()
+    })
+})
+
+const disabledBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true
+    }
+}
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false
+        box.innerText = ""
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, winner is ${winner}`
+    msgContainer.classList.remove("hide")
+    disabledBoxes()
+}
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText
+        let pos2Val = boxes[pattern[1]].innerText
+        let pos3Val = boxes[pattern[2]].innerText
+
+        if (pos1Val != "" && pos2Val != "" && pos2Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val)
+                return;
+            }
+        }
+    }
+    //DRAW 
+    // Count filled boxes
+    let filledCount = 0;
+    boxes.forEach(box => {
+        if (box.innerText !== "")
+            filledCount++;
+    });
+
+    // If all 9 boxes are filled and no winner, it's a draw
+    if (filledCount === 9) {
+        msg.innerText = "It's a Draw!";
+        msgContainer.classList.remove("hide");
+    }
+}
+
+
+newGameBtn.addEventListener("click", resetGame)
+resetBtn.addEventListener("click", resetGame)
