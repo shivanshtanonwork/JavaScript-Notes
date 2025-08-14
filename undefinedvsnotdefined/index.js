6f2a057c13813f3e18b92710dea258d2e75b5f5f
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-14 14:46:06 +0530

    undefined and not defined

---
+++ undefinedvsnotdefined/index.js
@@ -0,0 +1,13 @@
+console.log(a);
+var a = 15
+// console.log(x); //not defined
+
+console.log(a);
+
+if(a === undefined){
+    console.log("a is undefined");
+}else{
+    console.log("a is not undefined");
+}
+
+// a = undefined  never do this 
