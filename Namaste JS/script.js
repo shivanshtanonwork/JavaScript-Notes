011f322c785eabd00580888ff2465472779ef35d
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-09-25 12:43:28 +0530

    Closures in JS

--- Namaste JS/script.js
+++ Namaste JS/script.js
@@ -2,13 +2,24 @@
 // console.log(a);
 // var b = 100;
 
-var a = 100; //shadowing
-{
-  var a = 10;
-  let b = 20;
-  const c = 30;
-  console.log(a);
-  console.log(b);
-  console.log(c);
+// var a = 100; //shadowing
+// {
+//   var a = 10;
+//   let b = 20;
+//   const c = 30;
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+// console.log(a);
+
+function x() {
+  var a = 7;
+  function y() {
+    console.log(a);
+  }
+  return y;
 }
-console.log(a);
+var z = x();
+console.log(z);
+z();
