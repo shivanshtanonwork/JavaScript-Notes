9842d165c6aff56054d690f9ae82ce07b93ba4e0
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-09-25 11:23:39 +0530

    block scope and shadowing

--- Namaste JS/script.js
+++ Namaste JS/script.js
@@ -1,3 +1,14 @@
-let a = 10;
+// let a = 10;
+// console.log(a);
+// var b = 100;
+
+var a = 100; //shadowing
+{
+  var a = 10;
+  let b = 20;
+  const c = 30;
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
 console.log(a);
-var b = 100;
