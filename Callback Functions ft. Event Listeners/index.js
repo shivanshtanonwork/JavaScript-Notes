d080cd02a8fd1fa026db9d36e93e61a6fd75c656
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-10-20 11:18:26 +0530

    Callback Functions and Event listeners

---
+++ Callback Functions ft. Event Listeners/index.js
@@ -0,0 +1,17 @@
+// // Callback function
+// setTimeout(function () {
+//   console.log("timer");
+// }, 5000);
+
+// function x(y) {
+//   console.log("x");
+//   y();
+// }
+
+// x(function y() {
+//   console.log("y");
+// });
+
+document.getElementById("clickMe").addEventListener("click", function xyz() {
+  console.log("Btn is clicked");
+});
