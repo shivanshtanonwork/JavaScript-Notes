c3781317dac6c32b54af2e455e2603ee336f206a
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-10-23 13:14:56 +0530

    HOF map filter and reduce

--- Higher Order Functions/index.js
+++ Higher Order Functions/index.js
@@ -5,9 +5,40 @@
 // function y(x) {
 //   x();
 // }
-const radius = [3, 4, 5];
-const area = function (radius) {
-  return Math.PI * radius * radius;
-};
+// const radius = [3, 4, 5];
+// const area = function (radius) {
+//   return Math.PI * radius * radius;
+// };
 
-console.log(radius.map(area));
+// console.log(radius.map(area));
+const users = [
+  {
+    firstName: "Shivansh",
+    lastName: "Tandon",
+    age: 26,
+  },
+  {
+    firstName: "Aanvee",
+    lastName: "Sood",
+    age: 5,
+  },
+  {
+    firstName: "Palak",
+    lastName: "Sood",
+    age: 35,
+  },
+];
+
+let FullName = users.map((val) => val.firstName + " " + val.lastName);
+
+console.log(FullName);
+
+let totalAge = users.reduce((acc, curr) => {
+  return acc + curr.age;
+}, 0);
+
+console.log(totalAge);
+
+let canVote = users.filter((val) => val.age >= 18).map((x) => x.age);
+
+console.log(canVote);
