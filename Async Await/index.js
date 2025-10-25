603cf8b862c45b7b8c5e17f9efaadc621dcf9f48
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-10-25 11:39:41 +0530

    Deep dive Async Await

--- Async Await/index.js
+++ Async Await/index.js
@@ -15,23 +15,45 @@ data.then((res) => console.log(res));
 // //using promise
 // p.then((res) => console.log(res));
 
-let h1 = document.querySelector("h1");
+// let h1 = document.querySelector("h1");
 
-function changeColor(color, delay) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      h1.style.color = color;
-      console.log(`color changed to ${color}`);
-      resolve("Color changed");
-    }, delay);
-  });
+// function changeColor(color, delay) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       h1.style.color = color;
+//       console.log(`color changed to ${color}`);
+//       resolve("Color changed");
+//     }, delay);
+//   });
+// }
+
+// async function demo() {
+//   await changeColor("red", 2000);
+//   await changeColor("yellow", 2000);
+//   await changeColor("blue", 2000);
+//   await changeColor("green", 2000);
+// }
+
+// demo();
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise resolved");
+  }, 5000);
+});
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise resolved");
+  }, 2000);
+});
+
+async function handlePromise() {
+  const val = await p1;
+  console.log("Hello 1");
+  console.log(val);
+
+  const val2 = await p2;
+  console.log("Hello 2");
+  console.log(val2);
 }
-
-async function demo() {
-  await changeColor("red", 2000);
-  await changeColor("yellow", 2000);
-  await changeColor("blue", 2000);
-  await changeColor("green", 2000);
-}
-
-demo();
+handlePromise();
